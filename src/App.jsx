b165a613e0be0fb6364527c1a0b5ee77157a38f5
ParkGana <gnchoco97@gmail.com>
2024-11-26 20:28:26 +0900

    setting : tanstack query 설정하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { AuthProvider } from './contexts/AuthContext';
 import Router from './routes/Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <AuthProvider>
-            <Router />
-        </AuthProvider>
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <Router />
+            </AuthProvider>
+        </QueryClientProvider>
     );
 }
 
